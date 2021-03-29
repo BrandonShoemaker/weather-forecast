3d4c5dae4ec690dac053f5588b170078d51a3a01
BrandonShoemaker <brandon.shoemaker2080@gmail.com>
2021-03-28 23:00:29 -0500

    Finished card forecast update

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,52 +6,65 @@ function makeLowerCase(str){
 
 function getWeatherDataNormal(city, state, country) {
     city = makeLowerCase(city);
-    city[0] = city[0].toUpperCase();
-    state = makeLowerCase(city);
-    country = makeLowerCase(city);
+    city = city.charAt(0).toUpperCase()+city.slice(1);
+    state = state.toUpperCase();
+    country = country.toUpperCase();
     
     var apiUrl;
     if(!country){
         if(!state){
-            apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=1dbf052af59023935e40459b0107bc1a";
+            apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=1dbf052af59023935e40459b0107bc1a";
         }
         else{
-            apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state +"&appid=1dbf052af59023935e40459b0107bc1a";
+            apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "," + state +"&appid=1dbf052af59023935e40459b0107bc1a";
         }
     }
     else{
-        apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "," + country +"&appid=1dbf052af59023935e40459b0107bc1a";
+        apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "," + state + "," + country +"&appid=1dbf052af59023935e40459b0107bc1a";
     }
 
     fetch(apiUrl).then(response => {
+
         if(response.ok){
             return response.json();
         }
     })
-    then(data => {
+    .then(data => {
         console.log(data);
+        var midDayIncrementor = 4;
+        $("#card-container").children().each(function(){
+
+            var kelvinToFar = parseInt((((data.list[midDayIncrementor].main.temp)-273.15) * 9/5 + 32), 10);
+            var thisDiv = $(this);
+            thisDiv.find("h3").text(data.list[midDayIncrementor].dt_txt.substr(0,10));
+
+            console.log(data.list[midDayIncrementor].dt_txt.substr(0,10));
+
+            thisDiv.find("img").attr("src", "http://openweathermap.org/img/wn/"+ data.list[midDayIncrementor].weather[0].icon +"@2x.png");
+            thisDiv.find("img").attr("alt", data.list[midDayIncrementor].weather[0].description);
+
+            for (let i = 0; i < 2; i++) {
+                if(i === 0){
+                    thisDiv.find("p[data-pIndex='" + i + "']").html("Temperature: "+kelvinToFar+"&deg;");
+                }
+                else{
+                    thisDiv.find("p[data-pIndex='" + i + "']").text("Humidity: "+data.list[midDayIncrementor].main.humidity+"%");
+                }
+            }
+            midDayIncrementor += 8;
+        })
     })
 }
 
-function getWeatherDataZip(zip, country) {
-    
-}
-
-function getWeatherDataCityId(cityId) {
-    
-}
-
-function getWeatherDataCoordinates(lat, lon) {
-    
-}
-
 $(document).ready(function () {
-    $("#form-container").on("submit", function() {
+    $("#form-container").on("click", "button", function() {
         var location = $("#form-container").find("input").val().trim();
-        var locationParameters = location.split(",");
+        var locationParameters = location.split(" ");
         for (let i = 0; i < locationParameters.length; i++) {
-            locationParameters[i].trim();
+            if(locationParameters[i][locationParameters.length-1] === ",") locationParameters[i].splice(locationParameters.length-1, 1);
+            if()
         }
         if(locationParameters.length === 3) getWeatherDataNormal(locationParameters[0], locationParameters[1], locationParameters[2]);
+        
     });
 });
